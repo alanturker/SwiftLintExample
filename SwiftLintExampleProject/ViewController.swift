6d0747ab35a32151e90f9147c29b6c92d40f2af4
Turker Alan <turker.alan@huawei.com>
2022-03-17 09:33:42 +0300

    First Commit

---
+++ SwiftLintExampleProject/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftLintExampleProject
+//
+//  Created by admin on 9.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var productImage: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    private var array = ["shovel", "desk" , "pencil" ]
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+    }
+    
+}
+
+
